1669df8fedc969407dc0080fa5c18a01797aeb57
Ba-latte <youj_lim@naver.com>
2023-05-09 14:32:22 +0900

    5/9 오전 수업

--- my-vue-city-app/src/components/TopComp.vue
+++ my-vue-city-app/src/components/TopComp.vue
@@ -4,18 +4,31 @@
       <!-- 배열에서는 v : 값, i는 속성명 -->
       <!-- 👇리액티브 데이터인 뷰엑스 스토어 cityData변수를 컴포넌트 변수인 sdata에 할당하여 사용하면, 처음에만 할당된 변수로 세팅이 되고 cityData가 변경될 때 반영되지 않음! 따라서 리액티브 데이터인 실제 데이터를 직접 해당 자리에 사용해야함 -->
       <!-- <li v-for="(v, i) in sdata " v-bind:key="i" v-if="i!='인트로'"> -->
-      <li v-for="(v, i) in this.$store.state.cityData " v-bind:key="i" v-if="i!='인트로'">
+      <li
+        v-for="(v, i) in this.$store.state.cityData"
+        v-bind:key="i"
+        v-if="i != '인트로'"
+      >
         <a href="#" v-on:click="chgData(i)">{{ i }}</a>
       </li>
     </ul>
     <!-- 메뉴 선택 이동 링크 -->
     <div class="m2">
-      <a href="#" v-on:click="chgMenu(num)" v-text="'메뉴'+num"></a>
+      <!--
+        v-on:이벤트명.prevent
+        -기본기능 막기 옵션임
+        -.prevent는 event.preventDefault()와 같음!!
+        v-on:이벤트명.stop
+        -이벤트 버블링 막기 옵션임
+        -.stop는 event.stopPropagation()과 같음!!
+      -->
+      <a href="#" v-on:click.prevent="chgMenu(num)" v-text="'메뉴' + num"></a>
     </div>
   </header>
 </template>
 
 <script>
+import $ from 'jquery';
 export default {
   name: "TopArea",
   data() {
@@ -35,30 +48,72 @@ export default {
       this.$store.commit("chgData", pm);
     },
     // 메뉴 변경하기 메서드
-    chgMenu(n){
+    chgMenu(n) {
       // n - 메뉴번호 전달
       console.log("메뉴변경 : ", n);
       // 뮤테이션 메서드 호출하기!
-      this.$store.commit('chgMenu', n);
+      this.$store.commit("chgMenu", n);
 
       // 메뉴1/메뉴2 전환을 위한 변수 변경하기
       // 컴포넌트 변수인 num을 변경한다!
-      n===1? this.num=2 : this.num=1;
-    }
+      n === 1 ? (this.num = 2) : (this.num = 1);
+
+      // ⭐메뉴 변경시 DOM이 변경되므로 제이쿼리 메서드 호출하기!
+      // : 단! 제이쿼리 코드블록으로 감싸서 호출함으로써 DOM 로드 후 실행되도록 보장해줘야함
+      $(()=>this.setJQ());
+      // 즉! 돔이 먼저 생긴 후에 코드가 실행되도록, jQB블럭을 만들고 그 안에 this.setJG()메서드를 호출하면 돔이 생긴 후에 실행된다
+    },
+    // 제이쿼리 세팅 메서드 /////////////////////////
+    setJQ() {
+      console.log("jq세팅!");
+      // 링크 클릭시 a에 클래스 on 주기
+      $(".gnb a").click(function(e) {
+        e.preventDefault();
+        $(this)
+          .addClass("on")
+          .parent()
+          .siblings()
+          .find("a")
+          .removeClass("on");
+
+        // 박스 애니
+        showBx();
+      }); /////////////// click ///////////////////
+
+      // 이미지와 설명박스 순서대로 나타나기
+      function showBx() {
+        $("main img")
+          .css({ opacity: 0 })
+          .delay(500)
+          .stop()
+          .fadeTo(400, 1);
+
+        $("main p")
+          .css({ opacity: 0 })
+          .delay(1000)
+          .stop()
+          .fadeTo(400, 1);
+      } /////////////////// showBx //////////////////////
+    } //////////////// 제이쿼리 세팅 메서드 ////////////////////////
+  },
+  // DOM을 만들고 난 후 
+  mounted(){
+    // 제이쿼리 세팅 메서드 호출!
+    this.setJQ();
   }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.m2{
+.m2 {
   display: block;
   text-align: right;
 }
-.m2 a{
+.m2 a {
   font-size: 20px;
 }
-.m2 a:hover{
+.m2 a:hover {
   text-decoration: underline;
   text-decoration-style: double;
   color: green;
